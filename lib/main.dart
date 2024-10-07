d35098572bb6a8953fe89451ead0cc20e6bd2f12
thiareMohamed <thiaremohamed.mt@gmail.com>
2024-10-07 12:27:33 +0000

    Update qr_flutter and qr_code_scanner dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,123 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+
+  // Liste des widgets pour chaque page de la navbar
+  static List<Widget> _widgetOptions = <Widget>[
+    QrGeneratorWidget(),
+    QrScannerWidget(),
+    QrScannerWidget(),
+  ];
+
+  // Fonction appelée lors de la sélection d'un onglet
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('QR Code Generator'),
+          title: Text('QR Code App'),
         ),
-        body: Center(
-          // child: QrImage(
-          //   data: "https://flutter.dev",
-          //   version: QrVersions.auto,
-          //   size: 200.0,
-          // ),
-          child: Text('Hello flutter'),
+        body: _widgetOptions.elementAt(_selectedIndex),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.qr_code),
+              label: 'Générateur',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.qr_code_scanner),
+              label: 'Scanner',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.blue,
+          onTap: _onItemTapped,
         ),
       ),
     );
+  }
+}
+
+// Widget pour le générateur de QR code
+class QrGeneratorWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: QrImageView(
+        data: "https://flutter.dev",
+        version: QrVersions.auto,
+        size: 200.0,
+      ),
+    );
+  }
+}
+
+// Widget pour le scanner de QR code
+class QrScannerWidget extends StatefulWidget {
+  @override
+  _QrScannerWidgetState createState() => _QrScannerWidgetState();
+}
+
+class _QrScannerWidgetState extends State<QrScannerWidget> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: QRView(
+            key: qrKey,
+            onQRViewCreated: _onQRViewCreated,
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Center(
+            child: (result != null)
+                ? Text('Code Scanné: ${result!.code}')
+                : Text('Scan un code QR'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 }
